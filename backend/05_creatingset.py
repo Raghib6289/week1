3f920c1cb2b99b503972927bcb7c9383adf73e29
Md Raghib <kraghib123@gmail.com>
2026-07-13 21:09:32 +0530

    Update 05_creatingset.py

--- backend/05_creatingset.py
+++ backend/05_creatingset.py
@@ -16,6 +16,7 @@ my_list = [1, 2, 2, 3, 4, 4, 5]
 list_to_set = set(my_list)
 print(list_to_set) # Output: {1, 2, 3, 4, 5}
 
+
 # Creating a set from a string (each character becomes an element)
 string_set = set("hello")
 print(string_set) # Output: {'h', 'e', 'l', 'o'} (order may vary)
