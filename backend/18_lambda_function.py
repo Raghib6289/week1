ce11b9c49b49eff5b0fbd96c1510f7cc958cf642
Md Raghib <kraghib123@gmail.com>
2026-07-13 21:07:54 +0530

    Update 18_lambda_function.py

--- backend/18_lambda_function.py
+++ backend/18_lambda_function.py
@@ -37,6 +37,7 @@ print(squared_numbers) # Output: [1, 4, 9, 16, 25]
 
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
+
 even_numbers = list(filter(lambda x: x % 2 == 0, numbers))
 print(even_numbers) # Output: [2, 4, 6, 8, 10]
 
